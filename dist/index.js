5195b7a973d5a25e3952da35c9730d13c7a15062
mika <73530023+2everlove@users.noreply.github.com>
2021-02-06 18:46:12 +0900

    TS: Class(private, public)

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,33 @@
 //     age = 32,
 //     gender ="male";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "mika",
-    age: 22,
-    gender: "male"
-};
+// const sayHi = (name:string, age:number, gender:string): void => {
+//     console.log(`Hello ${name}, you are ${age}, a ${gender}`);
+// };
+// interface Human {
+//     name:string,
+//     age:number,
+//     gender:string
+// }
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    static getAge(age) {
+        return age;
+    }
+}
+// const person = {
+//     name : "mika",
+//     age : 22,
+//     gender : "male"
+// };
+const ahn = new Human("mika", 32, "male");
 const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, a ${person.gender}`;
+    return `Hello ${person.name}, you are ${Human.getAge(32)}, a ${person.gender}`;
 };
-console.log(sayHi(person));
+// console.log(sayHi(person));
+console.log(sayHi(ahn));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,40 @@
 //     console.log(`Hello ${name}, you are ${age}, a ${gender}`);
 // };
 
-interface Human {
-    name:string,
-    age:number,
-    gender:string
+// interface Human {
+//     name:string,
+//     age:number,
+//     gender:string
+// }
+
+class Human{
+    static getAge(age) {
+        return age;
+    }
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name : "mika",
-    age : 22,
-    gender : "male"
-};
+// const person = {
+//     name : "mika",
+//     age : 22,
+//     gender : "male"
+// };
+
+const ahn = new Human("mika", 32, "male");
 
 const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, a ${person.gender}`;
+    return `Hello ${person.name}, you are ${Human.getAge(32)}, a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+// console.log(sayHi(person));
+console.log(sayHi(ahn));
 
 export{};
 
